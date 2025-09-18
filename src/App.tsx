297b5425b074fa901419980ea578685e5cdf75ee
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2025-09-18 00:00:08 -0400

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -9,22 +9,42 @@ function App() {
     setColors([...colors, color]);
   }
 
+  function changeColor(color: string){
+    document.body.style.backgroundColor = color;
+  }
+
   return (
     <>
-      <h2>Color picker</h2>
-      <div>
-        <input type="text" value={color} onChange={(e) => setColor(e.target.value)}/>
-        <button onClick={() => setTask()}>Add color</button>
+      <div className='head'>
+        <header>
+          <h1>Color picker</h1>
+        </header>
+        <div className='add-color-section'>
+          <input 
+            type="text" 
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}
+            onKeyDown={(e) => { if (e.key === "Enter") setTask(); }}
+          />
+          <button 
+            onClick={() => setTask()}
+          >
+            Add color
+          </button>
+        </div>
       </div>
       <div className='colors-list'>
         {colors.map((color, index) => 
           <div className='color-element' key={index}>
+            <button 
+              className='color-btn' 
+              style={{ "--bg": color} as React.CSSProperties}  
+              onClick={() => changeColor(color)}>
+              </button>
             <p>{color}</p>
-            <div className='color-background' style={{backgroundColor: color}}></div>
           </div>
         )}
       </div>
-
     </>
   )
 }
